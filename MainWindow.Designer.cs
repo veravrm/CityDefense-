2204a310b6f4f7308703f9c9e6b29516512b184f
alena21romanovaa <romanovaalen17@mail.ru>
2023-12-16 01:51:23 +0300

    first-commit2/2
    Системные файлы игры содержат важные настройки, конфигурации и код, которые определяют работу игры. Они могут включать в себя файлы, управляющие геймплеем, графикой, звуком, сохранениями и многим другим.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CityDefense
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Frame = new System.Windows.Forms.Timer(this.components);
+            this.renderBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.renderBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Frame
+            // 
+            this.Frame.Enabled = true;
+            this.Frame.Interval = 40;
+            this.Frame.Tick += new System.EventHandler(this.Frame_Tick);
+            // 
+            // renderBox
+            // 
+            this.renderBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.renderBox.Location = new System.Drawing.Point(-3, -5);
+            this.renderBox.Name = "renderBox";
+            this.renderBox.Size = new System.Drawing.Size(806, 458);
+            this.renderBox.TabIndex = 0;
+            this.renderBox.TabStop = false;
+            this.renderBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.renderBox_MouseDown);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.renderBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainWindow";
+            this.Text = "City Defense";
+            ((System.ComponentModel.ISupportInitialize)(this.renderBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Frame;
+        private System.Windows.Forms.PictureBox renderBox;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CityDefense.Game;
+using CityDefense.Engine;
+
+namespace CityDefense
+{
+    public partial class MainWindow : Form
+    {
+        Scene scene;
+        public MainWindow()
+        {
+            InitializeComponent();            
+            
+            Render.SetResolution(renderBox.Width, renderBox.Height);
+            Render.SetScene(scene = new Scene());
+            
+            Time.SetInterval(Frame.Interval);
+            Frame.Tick += new System.EventHandler(Time.Frame_Tick);
+        }
+
+        private void Frame_Tick(object sender, EventArgs e)
+        {
+            renderBox.BackgroundImage = Render.DrawFrame();
+            Frame.Enabled = !GameOver.isGameOver;
+        }
+
+        private void renderBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            scene.BreakBomb(e.X, e.Y);
+        }
+    }
+}
